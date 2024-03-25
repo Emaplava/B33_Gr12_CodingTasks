71b00d49119b7e8d9ef3c4adef5858276cca714c
Yazid <yazid.at.1994@gmail.com>
2024-03-24 21:57:51 -0400

    task1-2-3

--- src/yazid/week1_tasks/finra.java
+++ src/yazid/week1_tasks/finra.java
@@ -1,4 +1,23 @@
 package yazid.week1_tasks;
 
+
 public class finra {
+    public static void main(String[] args) {
+
+        int number = 15;
+        String result = "";
+
+        if (number % 3 == 0 && number % 5 == 0) {
+            result = "FINRA";
+        } else if (number % 5 == 0) {
+            result = "RA";
+        } else if (number % 3 == 0) {
+            result = "FIN";
+        } else {
+            result = "" + number;
+        }
+
+        System.out.println("result = " + result);
+
+    }
 }
