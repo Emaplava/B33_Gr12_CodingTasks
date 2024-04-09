c73519869b71dcf11dadf39fe26ff708a02442ab
Ali <adeebsina@gmail.com>
2024-04-09 14:10:09 -0400

    My forth commit.

--- src/Fawad/week03_solutions/Number_Prime.java
+++ src/Fawad/week03_solutions/Number_Prime.java
@@ -3,12 +3,12 @@ package Fawad.week03_solutions;
 public class Number_Prime {
     public static void main(String[] args) {
 
-      int num = 50;
-        System.out.println(num);
+      int num = 50; // nums assigned value is 50 and
+        System.out.println(num); // print num
 
-        for (int i = 2; i <= num; i++) {
-            boolean isPrime = true;
-            for (int j = 2; j <= num; j++) {
+        for (int i = 2; i <= num; i++) { // we iterate through numbers from 2 because 2 is not a prime number
+            boolean isPrime = true; // this line initialize a boolean variable
+            for (int j = 2; j <= num; j++) { // This nested loop checks if the number i is dividable or not
                 if (i % j == 0) {
                     isPrime = false;
                     break;
