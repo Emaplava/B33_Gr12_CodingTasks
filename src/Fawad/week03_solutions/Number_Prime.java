070a7a6162f8b6ba43a86cc611250ac69431c656
Ali <adeebsina@gmail.com>
2024-04-09 14:18:38 -0400

    My forth commit.

--- src/Fawad/week03_solutions/Number_Prime.java
+++ src/Fawad/week03_solutions/Number_Prime.java
@@ -8,14 +8,14 @@ public class Number_Prime {
 
         for (int i = 2; i <= num; i++) { // we iterate through numbers from 2 because 2 is not a prime number
             boolean isPrime = true; // this line initialize a boolean variable
-            for (int j = 2; j <= num; j++) { // This nested loop checks if the number i is dividable or not
+            for (int j = 2; j < i; j++) { // This nested loop checks if the number i is dividable or not
                 if (i % j == 0) {
                     isPrime = false;
                     break; //
                 }
             }
             if (isPrime){
-                System.out.print(i + "");
+                System.out.print(i + " ");
             }
         }
     }
